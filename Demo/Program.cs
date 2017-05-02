798b8b1deb0a2c6d0cbc668c268b100f5e754564
Vandana Sharma <vandana.s@in.abb.com>
2017-05-02 17:23:34 +0530

    initial commit

---
+++ Demo/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    class Swapping
+
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2, temp;
+
+            num1 = 10;
+            num2 = 20;
+            temp = 30;
+
+            //Swapping of numbers
+            temp = num1;
+
+            num1 = num2;
+
+            num2 = temp;
+
+            Console.Write("\nAfter Swapping : ");
+
+            Console.Write("\nFirst Number : " + num1);
+
+            Console.Write("\nSecond Number : " + num2);
+
+            Console.Read();
+
+        }
+    }
+}
